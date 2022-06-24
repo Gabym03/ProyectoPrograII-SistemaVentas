50530755e0cf0f36ed1dfb509171d6fd2337e3cb
cvasquezg26 <100563946+cvasquezg26@users.noreply.github.com>
2022-06-23 18:43:18 -0600

    Nueva actualización de datos (ver descripción)
    Se eliminó: 
    /*
     * To change this license header, choose License Headers in Project Properties.
     * To change this template file, choose Tools | Templates
     * and open the template in the editor.
     */
    
    Se hicieron las siguientes importaciones:
    import config.Conexion;
    import java.sql.Connection;
    import java.sql.PreparedStatement;
    import java.sql.ResultSet;
    import java.util.ArrayList;
    import java.util.List;
    
    Se agrgó la siguiente información:
    
     Conexion cn = new Conexion();
        Connection con;
        PreparedStatement ps;
        ResultSet rs;
        int r;
       
        public Cliente buscar(String dni) {
            Cliente c = new Cliente();
            String sql = "Select * from cliente where Dni=" + dni;
            try {
                con = cn.Conexion();
                ps = con.prepareStatement(sql);
                rs = ps.executeQuery();
                while (rs.next()) {
                    c.setId(rs.getInt(1));
                    c.setDni(rs.getString(2));
                    c.setNom(rs.getString(3));
                    c.setDir(rs.getString(4));
                    c.setEs(rs.getString(5));
                }
            } catch (Exception e) {
            }
            return c;
        }

--- SistemaVentas/src/java/Modelo/ClienteDAO.java
+++ SistemaVentas/src/java/Modelo/ClienteDAO.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelo;
 
-/**
- *
- * @author gabri
- */
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClienteDAO {
-    
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    int r;
+   
+    public Cliente buscar(String dni) {
+        Cliente c = new Cliente();
+        String sql = "Select * from cliente where Dni=" + dni;
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                c.setId(rs.getInt(1));
+                c.setDni(rs.getString(2));
+                c.setNom(rs.getString(3));
+                c.setDir(rs.getString(4));
+                c.setEs(rs.getString(5));
+            }
+        } catch (Exception e) {
+        }
+        return c;
+    }
 }
+    

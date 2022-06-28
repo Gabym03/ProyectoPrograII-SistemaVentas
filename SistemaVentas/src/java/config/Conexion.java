8da77b9f165d1a67c2ff22a087bd110c340bad1c
Gabym03 <100321923+Gabym03@users.noreply.github.com>
2022-06-28 10:25:15 -0600

    MODIFICACION EN CONEXION

--- SistemaVentas/src/java/config/Conexion.java
+++ SistemaVentas/src/java/config/Conexion.java
@@ -2,11 +2,12 @@ package config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class Conexion {
     Connection con;
-    String url="jdbc:mysql://localhost:3306/bd_ventas";
+    String url="jdbc:mysql://127.0.0.1:3306/bd_ventas";
     String user="root";
     String pass="1234";
     public Connection Conexion(){
